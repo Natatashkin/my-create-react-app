000f32eb9d700974f002deaabc52d558b6e73210
Natatashkin <cs070283sng@gmail.com>
2022-07-20 01:31:19 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useForm, Controller } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
 import './App.css';
 
 function App() {
-  return <div className="App">My app</div>;
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: '',
+    },
+  });
+  const onSubmit = data => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="firstName"
+        control={control}
+        render={({ field }) => <TextField {...field} />}
+      />
+
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default App;
