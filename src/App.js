d32a350ee8ab95903a8874b39aa0888c82512d02
Natatashkin <cs070283sng@gmail.com>
2022-07-24 22:32:58 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import { useForm, Controller } from 'react-hook-form';
-import TextField from '@mui/material/TextField';
 import './App.css';
 
 function App() {
-  const { control, handleSubmit } = useForm({
-    defaultValues: {
-      firstName: '',
-    },
-  });
-  const onSubmit = data => console.log(data);
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="firstName"
-        control={control}
-        render={({ field }) => <TextField {...field} />}
-      />
-
-      <input type="submit" />
-    </form>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { StylesProvider, ThemeProvider } from '@mui/styles';
-import { jss, generateClassName } from './styles/helpers';
-import { theme } from './styles/theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StylesProvider jss={jss} generateClassName={generateClassName}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </StylesProvider>
+    <App />
   </React.StrictMode>,
 );
